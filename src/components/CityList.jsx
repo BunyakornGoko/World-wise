162393d956c3e5438a85e2e00a4ebd0720ce30a4
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-10-13 00:08:12 +0700

    create-cities-context

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,7 +2,10 @@ import CityItem from "./CityItem"
 import styles from "./CityList.module.css"
 import Spinner from "./Spinner"
 import Message from "./Message"
-export default function CityList({ cities, isLoading }) {
+import { useCities } from "../CitiesContext"
+
+export default function CityList() {
+  const { cities, isLoading } = useCities()
   if (isLoading) {
     return <Spinner />
   }
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,7 +2,11 @@ import CountryItem from "./CountryItem"
 import styles from "./CountryList.module.css"
 import Spinner from "./Spinner"
 import Message from "./Message"
-export default function CountryList({ cities, isLoading }) {
+import { useCities } from "../CitiesContext"
+
+export default function CountryList() {
+  const { cities, isLoading } = useCities()
+
   if (isLoading) {
     return <Spinner />
   }
